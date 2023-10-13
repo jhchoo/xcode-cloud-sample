c305cf10b813472d7e941d7a6aa9d3829a42dd79
jhchoo <jhchoo.kr@gmail.com>
2023-10-13 14:58:26 +0900

    1

---
+++ xcode-cloud-sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xcode-cloud-sample
+//
+//  Created by jae hwan choo on 10/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello,xcode cloud world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xcode-cloud-sample/xcode_cloud_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_cloud_sampleApp.swift
+//  xcode-cloud-sample
+//
+//  Created by jae hwan choo on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct xcode_cloud_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
